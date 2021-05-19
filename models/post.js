dcbb672e08920e657095afde43c93e60bdb8e4bb
Zakk Haug <zakklikeslamps@gmail.com>
2021-05-19 03:20:47 -0400

    Models complete

---
+++ models/post.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {} 
+
+Post.init(
+
+    {
+        id: {
+
+            type: DataTypes.INTEGER,
+            primaryKey: true, 
+            allowNull: false, 
+            autoIncrement: true
+
+        }, 
+
+        title: {
+
+            type: DataTypes.STRING,
+            allowNull: false, 
+
+        },
+
+        content: {
+
+            type: DataTypes.STRING, 
+
+        },
+
+        date_created: {
+
+            type: DataTypes.INTEGER, 
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+
+        }
+        
+    },
+
+    {
+        sequelize, 
+        timestamps: false,
+        freeTableName: true, 
+        underscored: true, 
+        modelName: 'post'
+    }
+
+);
+
+module.exports = Post;
---
+++ models/user.js
@@ -0,0 +1,78 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcruypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+
+    {
+        id: {
+
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            primaryKey: true, 
+            autoIncrement: true
+
+        },
+
+        name: {
+
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        },
+
+        email: {
+
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+
+        },
+
+        password: {
+
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [10],
+            }
+
+        }
+
+    },
+
+    {
+
+        //use hooks to hash users PW before storing new user info to DB
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+
+        sequelize, 
+        timestamps: false, 
+        freezeTableName: true,
+        underscored: true, 
+        modelName: 'user'
+
+    }
+
+);
+
+module.exports = User;
