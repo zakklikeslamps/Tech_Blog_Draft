5adb24364b20af9bafdf8343edc42f17c7e844ce
Zakk Haug <zakklikeslamps@gmail.com>
2021-05-18 22:00:09 -0400

    Connection & API routes established

---
+++ config/connectioin.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+require ('dontenv').config();
+
+let sequelize; 
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3308
+        } 
+
+    );
+
+}
+
+module.exports = sequelize;
---
+++ controllers/API/postRoutes.js
@@ -0,0 +1,79 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+const { post } = require('./commentRoutes');
+
+router.post('/', withAuth, async (req, res) => {
+
+    try {
+
+        const newPost = await Post.create({
+            ...req.body, 
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newPost);
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+
+    try {
+        const postData = await Post.update(
+
+            {
+                title: req.body.title,
+                content: req.body.content,
+            },
+
+            {
+                where: {
+                    id: req.params.id,
+                    user_id: req.sessioin.user_id,
+                },
+            }
+        );
+
+        if(!postData) {
+            res.status(404).json({message: 'No posts found with that id.'});
+
+            return; 
+        }
+
+        res.status(200).json(postData);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destry(
+
+            {
+                where: {
+                id: req.params.id,
+                user_id:req.session.user_id,
+                }
+            }
+        );
+
+        if (!postData) { res.status(404).json({message: 'No posts found with that id.'});
+        return; 
+        }
+
+        res.status(200).json(postData);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+module.exports = router; 
