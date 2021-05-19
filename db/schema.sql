97b8a8d7cc3009b6464c7744724e7f4737fa473c
Zakk Haug <zakklikeslamps@gmail.com>
2021-05-19 14:55:25 -0400

    All files written. Need to connect to db.

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
 DROP DATABASE IF EXISTS techblog_db;
 
 CREATE DATABASE techblob_db;
+
+USE techblob_db;
---
+++ utils/auth.js
@@ -0,0 +1,14 @@
+const withAuth = (req, res, next) => {
+
+    if (!req.session.logged_in) {
+
+        res.redirect('/login');
+
+    } else {
+
+        next();
+        
+    }
+};
+
+module.exports = withAuth;
