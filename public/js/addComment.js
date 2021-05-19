3e5135e0bfe34223216ac2bc5e6eff2ba0b3f6b4
Zakk Haug <zakklikeslamps@gmail.com>
2021-05-19 03:45:58 -0400

    public routes: still need login/logout routes

---
+++ public/js/addComment.js
@@ -0,0 +1,36 @@
+const addComment = async (event) => {
+
+    event.preventDefault();
+
+    const comment = document.querySelector('#user-comment').nodeValue.trim();
+    const post_id = window.location.toString().split('/') [ 
+        
+        window.location.toString().split('/').length - 1 
+    
+    ];
+
+    if (comment) {
+
+        const resonse = await fetch('api/comment', {
+
+            method: 'POST',
+            body: JSON.stringify({ comment, post_id }),
+            headers: { 'Content-Type': 'application/json'}
+
+        });
+
+        if (response.ok) {
+
+            document.location.reload();
+
+        } else {
+
+            alert(response.statusText);
+
+        }
+
+    }
+
+};
+
+document.querySelector('.add-comment').addEventListener('submit', addComment);
---
+++ public/js/editPost.js
@@ -0,0 +1,32 @@
+const editPost = async (event) => {
+
+    event.preventDefault();
+
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+    const id = window.location.toString().split('/') [ 
+
+        window.location.toString().split('/').length - 1 
+
+    ];
+
+    const response = await fetch (`/api/post/${id}`, {
+
+        method: 'PUT',
+        body: JSON.stringify({ 'Content-Type': 'application/json' })
+
+    });
+
+    if (response.ok) {
+
+        document.location.replace('/dashboard');
+
+    } else {
+
+        alert(respons.statusText);
+
+    }
+
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editPost);
